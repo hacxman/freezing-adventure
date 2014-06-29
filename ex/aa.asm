70812532c1f23ce2328fbabb2140d24220cdb59b
Maros Zatko <hacxman@gmail.com>
2014-06-29 15:54:11 +0200

    shuffle examples around

---
+++ ex/aa.asm
@@ -0,0 +1,2001 @@
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
+
+rofl:
+
+nop
+add %3 %8 %10
+sub %2 %9 %5
+
+mov %0 15
+mov %2 %0
+st    %2   [200]
+ld  %6 [2]
+
+asdfasdf:
+st    %4   asdfasdf
+
+ld %7 rofl
+
