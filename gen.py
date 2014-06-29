3503f097f869dbfd5421955ccdaf5984ee240a7a
Maros Zatko <hacxman@gmail.com>
2014-06-29 18:20:16 +0200

    speedup gen

--- gen.py
+++ gen.py
@@ -2,35 +2,38 @@ import random
 from operator import add
 import sys
 
-lb = [reduce(add, [chr(random.randrange(ord('a'), ord('z'))) for l in range(random.randrange(5,7))], '') for n in range(2 + int(sys.argv[1])/200)]
+lb = [reduce(add, [chr(random.randrange(ord('a'), ord('z'))) for l in range(random.randrange(5,7))], '') for n in xrange(2 + int(sys.argv[1])/200)]
 
 def geni():
-    l = lb[random.randrange(0, len(lb))]
-    r0, r1, r2 = random.randrange(0, 8), random.randrange(0, 8), random.randrange(0, 8)
-    rval = random.randrange(0, 256)
-    r2 = random.random()
-
+    rc = random.random()
     r = random.random()
-    if r2 < 0.3:
+    r0 = random.randrange(0, 8)
+    if rc < 0.3:
       sys.stdout.write("c")
     if r < 0.1:
       print "nop"
     elif r >= 0.1 and r < 0.2:
+      l = lb[random.randrange(0, len(lb))]
       print "ld %%%i" % r0, l
     elif r >= 0.2 and r < 0.4:
+      l = lb[random.randrange(0, len(lb))]
       print "st %%%i" % r0, l
     elif r >= 0.4 and r < 0.6:
+      r1 = random.randrange(0, 8)
       print "mov %%%i %%%i" % (r0, r1)
     elif r >= 0.6 and r < 0.8:
+      rval = random.randrange(0, 256)
       print "mov %%%i" % (r0), rval
     elif r >= 0.8 and r < 0.9:
+      r1, r2 = random.randrange(0, 8), random.randrange(0, 8)
       print 'add %%%i %%%i %%%i' % (r0, r1, r2)
     elif r >= 0.9 and r < 1:
+      r1, r2 = random.randrange(0, 8), random.randrange(0, 8)
       print 'sub %%%i %%%i %%%i' % (r0, r1, r2)
 
 def genl():
     l =  lb[random.randrange(0, len(lb))]
     print "%s:" % l
 
-[geni() if random.random() > 0.2 else genl() for x in range(int(sys.argv[1]))]
+[geni() if random.random() > 0.2 else genl() for x in xrange(int(sys.argv[1]))]
 
